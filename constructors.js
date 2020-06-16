bc97dd7c2c600608bf82b30b6e8c565eafd35554
bishopwalker <walkerbishop333@gmail.com>
2020-06-16 07:19:57 -0700

    finally finished the last two problems

--- constructors.js
+++ constructors.js
@@ -89,7 +89,13 @@ function Movie(name, genre, rating) {
 
 // Code here
 Movie.prototype.changeRating = function (rating) {
-  return (this.rating + rating) / 2
+
+  if (rating > 0 && rating < 101) {
+    this.rating = (this.rating + rating) / 2
+
+  }
+  console.log(this.rating);
+  return this.rating;
 }
 let twister = new Movie('twister', 'shitty', 6.9);
 console.log(twister.changeRating(7.8));
@@ -105,28 +111,49 @@ console.log(twister.changeRating(7.8));
 // Code here
 
 
-function User(name, age, email, savedPost) {
+function User(name, age, email, savedPosts) {
   this.name = name;
   this.age = age;
   this.email = email;
-  this.savedPost = savedPost;
+  this.savedPosts = savedPosts;
 }
 
-User.prototype.addSavedPost=(id,title,rating)=>{
-  (rating>0 && rating<6)?rating=rating:5;
-  return this.id + " " + "" title + rating
-
+User.prototype.addSavedPost = function (id, title, rating) {
+  let newObj = {
+    id,
+    title,
+    rating,
+  }
+  this.savedPosts.push(newObj)
 }
+
+let b = new User('vv', 3, '33', ['3', 3, 4]);
+b.addSavedPost(3, 'thanks', 4);
+console.log(b)
 ////////// PROBLEM 6 //////////
 
-  // You will be using the constructor function you just created in problem 5.
-  // Write a prototype method for the User constructor function named removeSavedPost that will take in one number parameter representing the post id. Use this id to find and remove the matching object in the savedPosts array.
+// You will be using the constructor function you just created in problem 5.
+// Write a prototype method for the User constructor function named removeSavedPost that will take in one number parameter representing the post id. Use this id to find and remove the matching object in the savedPosts array.
 
-  // Code here
+// Code here
+User.prototype.removeSavedPost = function (id) {
+  let index = this.savedPosts.findIndex(e => {
+    return e.id === id
+  });
+  this.savedPosts.splice(index, 1)
+}
+////////// PROBLEM 7 //////////
 
-  ////////// PROBLEM 7 //////////
+// You will continue to use the constructor function you created in problem 5.
+// Write a prototype method for the User constructor function named changePostRating that will take in two number parameters. The first will be an id (a number) and the second will be the new rating (a number). Use the id to find the matching object in the savedPosts array. Once you find the matching object, update it's rating score with the new rating parameter.
 
-  // You will continue to use the constructor function you created in problem 5.
-  // Write a prototype method for the User constructor function named changePostRating that will take in two number parameters. The first will be an id (a number) and the second will be the new rating (a number). Use the id to find the matching object in the savedPosts array. Once you find the matching object, update it's rating score with the new rating parameter.
-
-  // Code here
+// Code here
+User.prototype.changePostRating = function (id, rating) {
+  //the key from the parameter
+  let index = this.savedPosts.findIndex(element => {
+    return element.id === id
+  })
+  this.savedPosts[index].rating = rating
+  console.log((this.savedPosts))
+}
+b.changePostRating(33, 420);
